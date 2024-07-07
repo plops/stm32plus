2b71b4f47cb7962f78ff52a5a43e7beea82d98a6
Martin Kielhorn <wolpumba@gmail.com>
2024-07-07 20:13:27 +0200

    Is this an okay selection of files? Appending all leaves a 3.9M context

--- doc/01_collect_files.sh
+++ doc/01_collect_files.sh
@@ -6,7 +6,31 @@
 # Limit implementation that exists for multiple hardware devices to stm32f405
 
 # Recursively find all files in ../ that are not too large and not in the .git directory
-# and have file ending *.cpp, *.h or *.md. Store in a variable and print
+# and have file ending *.cpp, *.h or *.md.
 
-files = $(find .. -type f -not -path '../.git/*' -not -path '../doc/*' -not -path '../data/*' -not -path '../models/*' -not -path '../.vscode/*' -not -path '../.github/*' -not -path '../.gitignore' -not -path '../README.md' -not -path '../LICENSE' -not -path '../.gitmodules' -not -path '../.gitattributes
+find .. -type f \
+    -not -path '../.git/*' \
+    -not -path '*/f0/*' \
+    -not -path '*/f1/*' \
+    -not -path '../lib/include/display/*' \
+    -not -path '../lib/src/display/*' \
+    -not -path '../utils/liblzg/*' \
+    -not -path '../lib/fwlib/f4/cmsis/*' \
+    -not -path '../lib/include/dma/f4/*' \
+    -not -path '../lib/include/stl/*' \
+    -size -50k -regex ".*\.\(cpp\|h\|md\)" > 01a_files.txt
 
+# Print all files in 01a_files.txt with their size and sorted by size
+ls -l $(cat 01a_files.txt) | awk '{print $5, $9}' | sort -n
+
+
+
+# Merge all files into one large file to generate context
+# Prefix every file with "// START FILE: <filename>"
+# and suffix with "// END FILE: <filename>"
+
+cat 01a_files.txt | while read file; do
+    echo "// START FILE: $file"
+    cat $file
+    echo "// END FILE: $file"
+done > 01b_all_files.txt
